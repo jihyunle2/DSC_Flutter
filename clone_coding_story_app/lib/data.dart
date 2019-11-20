58e86672aa51351a73c8346f67a51ce35277bcaf
jihyunle2 <ljh3795@naver.com>
2019-11-20 20:41:33 +0900

    add clone story app project

---
+++ clone_coding_story_app/lib/data.dart
@@ -0,0 +1,17 @@
+List<String> images = [
+  "assets/image_06.jpg",
+  "assets/image_05.jpg",
+  "assets/image_04.jpg",
+  "assets/image_03.jpg",
+  "assets/image_02.jpg",
+  "assets/image_01.jpg",
+];
+
+List<String> title = [
+  "펭러뷰",
+  "Pengsoo",
+  "펭펭",
+  "Fallen In Love",
+  "펭하",
+  "PengPeng",
+];
