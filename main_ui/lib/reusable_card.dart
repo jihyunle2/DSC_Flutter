c159519c55fa9ffca3af6473258f3f6e580bd011
jihyunle2 <ljh3795@naver.com>
2020-01-24 15:12:18 +0900

    resize card box at main ui

--- main_ui/lib/reusable_card.dart
+++ main_ui/lib/reusable_card.dart
@@ -4,33 +4,46 @@ class ReusableCard extends StatelessWidget{
   ReusableCard({@required this.colour});
 
   final Color colour;
+  static const mainTextStyle = TextStyle(
+    fontSize: 15.0,
+  );
 
   @override
   Widget build(BuildContext context){
     return Container(
       child: Row(
-       
+        mainAxisAlignment: MainAxisAlignment.center,
         children: <Widget>[
           Expanded(
             flex: 1,
-            child: Text('20'),
+            child: Text(
+              '20',
+              style: mainTextStyle,
+            ),
           ),
           Expanded(
             flex: 4,
-            child: Text('새벽 4시에 잤다.'),
+            child: Text(
+              '새벽 4시에 잤다.',
+              style: mainTextStyle,
+            ),
           ),
           Expanded(
             flex: 1,
-            child: Text('+'),
+            child: Text(
+              '+',
+              style: mainTextStyle,
+            ),
           ),
         ],
-        
       ),
-      margin: EdgeInsets.all(15.0),
-        decoration: BoxDecoration(
-          color: colour,
-          borderRadius: BorderRadius.circular(10.0),
-        ),
+      margin: EdgeInsets.all(5.0),
+      height: 30.0,
+      padding: EdgeInsets.all(5.0),
+      decoration: BoxDecoration(
+        color: colour,
+        borderRadius: BorderRadius.circular(10.0),
+      ),
     );
   }
 }
