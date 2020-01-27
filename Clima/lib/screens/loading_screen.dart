ae9952660282d31bf340d79c4cbc227f1f4471ff
jihyunle2 <ljh3795@naver.com>
2020-01-27 23:36:15 +0900

    use geolocator's latitude and longitude

--- Clima/lib/screens/loading_screen.dart
+++ Clima/lib/screens/loading_screen.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:geolocator/geolocator.dart';
-
+import 'package:clima/services/location.dart';
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -8,22 +7,23 @@ class LoadingScreen extends StatefulWidget {
 }
 
 class _LoadingScreenState extends State<LoadingScreen> {
-  
-  void getLocation() async{
-    Position position = await Geolocator().getCurrentPosition(desiredAccuracy: LocationAccuracy.low);
+  @override
+  void initState(){
+    super.initState();
+    getLocation();
   }
+  void getLocation() async{
+    Location location = Location();
+
+    await location.getCurrentLocation();
+  }
+
+  
   
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: RaisedButton(
-          onPressed: () {
-            getLocation();
-          },
-          child: Text('Get Location'),
-        ),
-      ),
+      
     );
   }
 }
