6450b4e15dad89f605e25894c89068f7cc706e80
jihyunle2 <ljh3795@naver.com>
2020-02-14 01:13:54 +0900

    make task model and add checkbox fn

--- todoey_flutter/lib/widgets/tasks_list.dart
+++ todoey_flutter/lib/widgets/tasks_list.dart
@@ -1,16 +1,35 @@
 import 'package:flutter/material.dart';
 import 'package:todoey_flutter/widgets/tast_tile.dart';
+import 'package:todoey_flutter/models/task.dart';
 
-class TasksList extends StatelessWidget {
+class TasksList extends StatefulWidget {
+  @override
+  _TasksListState createState() => _TasksListState();
+}
+
+class _TasksListState extends State<TasksList> {
+
+  List<Task> tasks = [
+    Task(name: 'Buy milk'),
+    Task(name: 'sd'),
+    Task(name: 'hi'),
+  ];
 
   @override
   Widget build(BuildContext context) {
-    return ListView(
-      children: <Widget>[
-        TaskTile(),
-        TaskTile(),
-        TaskTile(),
-      ],
+    return ListView.builder(
+      itemBuilder: (context, index){
+        return TaskTile(
+          taskTitle: tasks[index].name,
+          isChecked: tasks[index].isDone,
+          checkboxCallback: (bool checkboxState){
+            setState(() {
+              tasks[index].toggleDone();
+            }); 
+          },
+        );
+      }, 
+      itemCount: tasks.length,
     );
   }
 }
